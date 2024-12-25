e75673f94e46f0ec77fe4e9e82e73c7e5c523b6b
Gaurav Kumar <mr.gauravkumar8757@gmail.com>
2024-12-25 17:37:05 +0530

    updates

--- frontend/src/config/config.jsx
+++ frontend/src/config/config.jsx
@@ -1,11 +1,6 @@
 // config.js
-<<<<<<< HEAD
-//const VITE_DATA = 'http://localhost:7000';
- const VITE_DATA = 'https://api.eleedomimf.com';
-=======
 // const VITE_DATA = 'http://localhost:7000';
 const VITE_DATA = 'https://api.eleedomimf.com';
->>>>>>> 6edac604acfe44f9bac76efb2fa4140ab4d53f9f
 export default VITE_DATA;
 
 
